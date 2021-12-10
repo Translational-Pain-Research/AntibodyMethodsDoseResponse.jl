cc68505b5b41a6e3d7ad65b7a9b98e529aeb025d
Dominik Tschimmel <dominik.tschimmel.contact@gmail.com>
2021-12-10 16:31:15 +0100

    Initialize Package

---
+++ src/AntibodyMethodsDoseResponse.jl
@@ -0,0 +1,5 @@
+module AntibodyMethodsDoseResponse
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AntibodyMethodsDoseResponse
+using Test
+
+@testset "AntibodyMethodsDoseResponse.jl" begin
+    # Write your tests here.
+end
